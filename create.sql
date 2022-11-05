1776b32c1aa0c6aa60eb1be1d19d69bcffc6a1bc
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-05 14:23:37 +0900

    Create create.sql

---
+++ create.sql
@@ -0,0 +1,89 @@
+CREATE TABLE Genre(
+    genre_id INT AUTO_INCREMENT PRIMARY KEY,
+    genre_name VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE Category(
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(20) NOT NULL
+)
+
+CREATE Director(
+    director_id INT AUTO_INCREMENT PRIMARY KEY,
+    director_name VARCHAR(50) NOT NULL
+)
+
+CREATE Distributor(
+    distributor_id INT AUTO_INCREMENT PRIMARY KEY,
+    distributor_name VARCHAR(40) NOT NULL
+)
+
+CREATE TABLE Festival(
+    festival_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT,
+    festival_name VARCHAR(30) NOT NULL,
+    continent VARCHAR(20) NOT NULL,
+    country VARCHAR(20) NOT NULL,
+    city VARCHAR(30),
+    FOREIGN KEY (category_id) REFERENCES Category(category_id)
+)
+
+CREATE TABLE Theater(
+    theater_id INT AUTO_INCREMENT PRIMARY KEY,
+    theater_name VARCHAR(20) NOT NULL,
+    branch VARCHAR(20) NOT NULL,
+    hall_num INT,
+    seat_num INT
+)
+
+CREATE TheaterAddress(
+    theater_id INT PRIMARY KEY,
+    city VARCHAR(30) NOT NULL,
+    district VARCHAR(30) NOT NULL,
+    FOREIGN KEY (theater_id) REFERENCES Theater(theater_id)
+)
+
+CREATE Movie(
+    movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    genre_id INT,
+    category_id INT,
+    distributor_id INT,
+    director_id INT,
+    movie_name VARCHAR(50) NOT NULL,
+    country VARCHAR(20),
+    released_date DATE NOT NULL,
+    film_rating VARCHAR(20),
+    FOREIGN KEY (genre_id) REFERENCES Genre(genre_id),
+    FOREIGN KEY (director_id) REFERENCES Director(director_id),
+    FOREIGN KEY (category_id) REFERENCES Category(category_id),
+    FOREIGN KEY (distributor_id) REFERENCES Distributor(distributor_id)
+)
+
+CREATE ScreeningInfo(
+    movie_id INT PRIMARY KEY,
+    screen_num INT,
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+)
+
+CREATE Sales(
+    movie_id INT PRIMARY KEY,
+    sales_total INT NOT NULL,
+    sales_seoul INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+)
+
+CREATE Spectator(
+    movie_id INT PRIMARY KEY,
+    spectator_total INT NOT NULL,
+    spectator_seoul INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+)
+
+CREATE Award(
+    award_id INT AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT NOT NULL,
+    festival_id INT NOT NULL,
+    award_year INT NOT NULL
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+    FOREIGN KEY (festival_id) REFERENCES Festival(festival_id)
+)

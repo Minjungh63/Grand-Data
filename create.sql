5f54f2eb7e604d2690ac68ce66ada07883dcd20e
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-07 02:30:40 +0900

    fix syntax error + add use database

--- create.sql
+++ create.sql
@@ -1,3 +1,5 @@
+USE TEAM11;
+
 CREATE TABLE Genre(
     genre_id INT AUTO_INCREMENT PRIMARY KEY,
     genre_name VARCHAR(20) NOT NULL
@@ -6,17 +8,17 @@ CREATE TABLE Genre(
 CREATE TABLE Category(
     category_id INT AUTO_INCREMENT PRIMARY KEY,
     category_name VARCHAR(20) NOT NULL
-)
+);
 
 CREATE TABLE Director(
     director_id INT AUTO_INCREMENT PRIMARY KEY,
     director_name VARCHAR(50) NOT NULL
-)
+);
 
-CREATE  TABLE Distributor(
+CREATE TABLE Distributor(
     distributor_id INT AUTO_INCREMENT PRIMARY KEY,
     distributor_name VARCHAR(40) NOT NULL
-)
+);
 
 CREATE TABLE Festival(
     festival_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -26,7 +28,7 @@ CREATE TABLE Festival(
     country VARCHAR(20),
     city VARCHAR(30),
     FOREIGN KEY (category_id) REFERENCES Category(category_id)
-)
+);
 
 CREATE TABLE Theater(
     theater_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -34,14 +36,14 @@ CREATE TABLE Theater(
     branch VARCHAR(20) NOT NULL,
     hall_num INT,
     seat_num INT
-)
+);
 
 CREATE TABLE Theater_Address(
     theater_id INT PRIMARY KEY,
     city VARCHAR(30) NOT NULL,
     district VARCHAR(30) NOT NULL,
     FOREIGN KEY (theater_id) REFERENCES Theater(theater_id)
-)
+);
 
 CREATE TABLE Movie(
     movie_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -57,33 +59,33 @@ CREATE TABLE Movie(
     FOREIGN KEY (director_id) REFERENCES Director(director_id),
     FOREIGN KEY (category_id) REFERENCES Category(category_id),
     FOREIGN KEY (distributor_id) REFERENCES Distributor(distributor_id)
-)
+);
 
 CREATE TABLE Screening_Info(
     movie_id INT PRIMARY KEY,
     screen_num INT,
     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
-)
+);
 
 CREATE TABLE Sales(
     movie_id INT PRIMARY KEY,
     sales_total INT NOT NULL,
     sales_seoul INT NOT NULL,
     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
-)
+);
 
 CREATE TABLE Spectator(
     movie_id INT PRIMARY KEY,
     spectator_total INT NOT NULL,
     spectator_seoul INT NOT NULL,
     FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
-)
+);
 
 CREATE TABLE Award(
     award_id INT AUTO_INCREMENT PRIMARY KEY,
     movie_id INT NOT NULL,
     festival_id INT NOT NULL,
-    award_year INT NOT NULL
-    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id)
+    award_year INT NOT NULL,
+    FOREIGN KEY (movie_id) REFERENCES Movie(movie_id),
     FOREIGN KEY (festival_id) REFERENCES Festival(festival_id)
-)
+);

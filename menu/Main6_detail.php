801a62a3dc150f3f761fd6e31ab063b2ae408f95
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-11-10 13:43:11 +0900

    view all festivals

---
+++ menu/Main6_detail.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<style>
+  #list_table{
+    text-align:center; 
+    margin-left:auto;
+    margin-right:auto;
+  }
+  .list_tr{
+    font-size:20px; 
+    font-weight:700; 
+    height:70px; 
+  }
+  .normal_tr{
+    height:40px; 
+    font-weight:700;
+  }
+</style>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <title>Grand Data</title>
+    <link rel="stylesheet" href="Main.css">
+    </head>
+
+    <body>
+        
+      <div id="updeco">
+        <a href="menu.html">Film Culture Industry Analysis: What makes a movie successful</a>
+      </div>
+
+      
+    <nav role="navigation">
+      <ul id="main-menu">
+        <li><a href="../menu/Main1.html">main1</a></li>
+        <li><a href="../menu/Main2.html">main1</a></li>
+        <li><a href="../menu/Main3.html">main3</a></li>
+        <li><a href="../menu/Main4.html">main4</a></li>
+        <li><a href="../menu/Main5.html">main5</a></li>
+        <li><a href="../menu/Main6.html">main6</a></li>
+        <li><a href="../menu/Main7.php">main7</a></li>
+      </ul>
+    </nav>
+
+
+<section>
+    <div id = "contents">
+    <h2 id="title">Film Festivals</h2>
+      <p>
+        Please choose the criteria by which you want to sort the film festivals.
+      </p>
+      <form action="Main6.php" method="post">
+       <select id="dropbox" name="sorting">
+         <option value="category" selected>Category
+         <option value="continent">Continent
+         <option value="country">Country
+         <option value="reality">Reality(Correlation between award winning films and films actually loved)
+       </select>
+       <input id="search" type="submit" value="search">
+      </form>
+      <?php
+      $mysqli = mysqli_connect("localhost", "team11", "team11", "team11");
+      if(mysqli_connect_errno()){
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+    else{
+        $sql = "SELECT * FROM Festival JOIN Category USING(category_id)";
+        $res = mysqli_query($mysqli, $sql);
+        if($res){
+            printf("<table id=\"list_table\">");
+            printf("<tr class=\"list_tr\"><td><B> name </B></td><td> category </td><td> continent </td><td> country </td><td> city </td></tr>");
+            while($newArray = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+                $festival_name = $newArray['festival_name'];
+                $category_name = $newArray['category_name'];
+                $continent = $newArray['continent'];
+                $country = $newArray['country'];
+                $city = $newArray['city'];
+                printf("<tr class=\"normal_tr\"><td><B> %s </B></td><td> %s </td><td> %s </td><td> %s </td><td> %s </td>",$festival_name, $category_name, $continent, $country, $city);
+            }
+            printf("</table><br>");
+        }
+        else{
+            printf("Could not retrieve records: %s\n", mysqli_error($mysqli));
+        }
+        mysqli_free_result($res);
+        mysqli_close($mysqli);
+    }
+?>
+
+    </div>
+</section>
+<div id="downdeco">
+Copyright &copy; GRAND_DATA_2022_All Rights Reserved. 
+</div>
+</body >
+</html> 

7853f396fa63986de9ef595a2cce52ec1a8c3eb2
suhhyun <2835223@ewhain.net>
2022-11-16 21:22:59 +0900

    Update title

--- menu/Main5_detail.php
+++ menu/Main5_detail.php
@@ -22,14 +22,14 @@
   }
 </style>
 <html>
-    <?php 
-      $name = $_GET['director_name'];
-      $id = $_GET['director_id'];
-      $menu = $_GET['state']=="search" ? "0" : $_POST['menu'];
-      ?>
+    <?php
+    $name = $_GET['director_name'];
+    $id = $_GET['director_id'];
+    $menu = $_GET['state'] == 'search' ? '0' : $_POST['menu'];
+    ?>
     <head>
       <meta charset="UTF-8">
-      <title>Grand Data</title>
+      <title>Film Culture Industry Analysis: What makes a movie successful</title>
       <link rel="stylesheet" href="Main.css">
     </head>
 
@@ -55,11 +55,11 @@
 
 <section>
     <div id = "contents">
-    <h2 id='title'><?php echo $_GET['director_name'];?></h2>
-    <p>This is the page of the director <?php echo $_GET['director_name'];?>.
+    <h2 id='title'><?php echo $_GET['director_name']; ?></h2>
+    <p>This is the page of the director <?php echo $_GET['director_name']; ?>.
     <br>You can search for an analysis of the director's filmography.
     <br><B>Please select the category where you want to receive the analysis.</B></p>
-    <form action="Main5_detail.php?director_id=<?php echo $id;?>&&director_name=<?php echo $name;?>&&state=result" method="post" style="margin-bottom:5%">
+    <form action="Main5_detail.php?director_id=<?php echo $id; ?>&&director_name=<?php echo $name; ?>&&state=result" method="post" style="margin-bottom:5%">
        <select id="dropbox" name="menu">
          <option value="0" selected>Movie List 
          <option value="1">Screening Number
@@ -69,52 +69,74 @@
        <input id="search" type="submit" value="search">
    </form>
       <?php
-        $mysqli=mysqli_connect("localhost","team11", "team11","team11");
-        if(mysqli_connect_errno()){
-            printf("Connect failed:%s\n",mysqli_connect_error());
-            exit();
-        }else{
-            if($menu=='1'){
-                $sql = "SELECT AVG(screen_num) as c1, MAX(screen_num) as c2 FROM movie JOIN screening_info USING(movie_id) JOIN director USING(director_id) WHERE director_id=".$_GET['director_id']." GROUP BY director_id";
-                $title = "🎞 Screening Number 🎞";
-                $head = "<tr class='head_tr'><th>Average</th><th>Max</th></tr>";
-              } else if($menu=='2') {
-                $sql = "SELECT AVG(spectator_total) as c1, AVG(spectator_seoul) as c3, MAX(spectator_total) as c2, MAX(spectator_seoul) as c4 FROM movie JOIN spectator USING(movie_id) JOIN director USING(director_id) WHERE director_id=".$_GET['director_id']." GROUP BY director_id";
-                $title = "🎞 Spectator Number 🎞";
-                $head = "<tr class='head_tr'><th>Average<br>*Total*</th><th>Max<br>*Total*</th><th>Average<br>*Seoul*</th><th>Max<br>*Seoul*</th</tr>";
-              }else if($menu=='3') {
-                $sql = "SELECT AVG(sales_total) as c1, AVG(sales_seoul) as c3, MAX(sales_total) as c2, MAX(sales_seoul) as c4 FROM movie JOIN sales USING(movie_id) JOIN director USING(director_id) WHERE director_id=".$_GET['director_id']." GROUP BY director_id";
-                $title = "🎞 Sales of Films 🎞";
-                $head = "<tr class='head_tr'><th>Average<br>*Total*</th><th>Max<br>*Total*</th><th>Average<br>*Seoul*</th><th>Max<br>*Seoul*</th</tr>";
-              }else{
-                $sql = "SELECT movie_name as c1, released_date as c2 FROM movie JOIN director USING(director_id) WHERE director_id=".$_GET['director_id']." ORDER BY released_date DESC";
-                $title = "🎞 List of Films 🎞";
-                $head = "<tr class='head_tr'><th>Title</th><th>Released Date</th></tr>";
-              }
-            $res = mysqli_query($mysqli,$sql);
-            $director_list = mysqli_fetch_array($res,MYSQLI_ASSOC);
-            if($res){
-                printf("<h2 class='toCenter'>%s</h2>",$title);
-                printf("<table class=\"toCenter\">%s",$head);
-                $i=0;
-                    do{
-                      $c1 = $director_list["c1"];
-                      $c2 = $director_list["c2"];
-                      printf("<tr class=\"normal_tr\"><td style=\"width:400px\"><B> %s </B></td><td style=\"width:400px\"> %s </td>",$c1, $c2);
-                      if((int)$menu>1) {
-                        $c3 = $director_list["c3"];
-                        $c4 = $director_list["c4"];
-                        printf("<td style=\"width:400px\"><B> %s </B></td><td style=\"width:400px\"> %s </td>",$c3, $c4);
-                      }
-                    }while($director_list = mysqli_fetch_array($res,MYSQLI_ASSOC));
-                
-                printf("</table>");
-            } else{
-            printf("Could not get the ranking of directors: %s\n", mysqli_error($mysqli));
-            }
-            mysqli_close($mysqli);
+      $mysqli = mysqli_connect('localhost', 'team11', 'team11', 'team11');
+      if (mysqli_connect_errno()) {
+        printf("Connect failed:%s\n", mysqli_connect_error());
+        exit();
+      } else {
+        if ($menu == '1') {
+          $sql =
+            'SELECT AVG(screen_num) as c1, MAX(screen_num) as c2 FROM movie JOIN screening_info USING(movie_id) JOIN director USING(director_id) WHERE director_id=' .
+            $_GET['director_id'] .
+            ' GROUP BY director_id';
+          $title = '🎞 Screening Number 🎞';
+          $head = "<tr class='head_tr'><th>Average</th><th>Max</th></tr>";
+        } elseif ($menu == '2') {
+          $sql =
+            'SELECT AVG(spectator_total) as c1, AVG(spectator_seoul) as c3, MAX(spectator_total) as c2, MAX(spectator_seoul) as c4 FROM movie JOIN spectator USING(movie_id) JOIN director USING(director_id) WHERE director_id=' .
+            $_GET['director_id'] .
+            ' GROUP BY director_id';
+          $title = '🎞 Spectator Number 🎞';
+          $head =
+            "<tr class='head_tr'><th>Average<br>*Total*</th><th>Max<br>*Total*</th><th>Average<br>*Seoul*</th><th>Max<br>*Seoul*</th</tr>";
+        } elseif ($menu == '3') {
+          $sql =
+            'SELECT AVG(sales_total) as c1, AVG(sales_seoul) as c3, MAX(sales_total) as c2, MAX(sales_seoul) as c4 FROM movie JOIN sales USING(movie_id) JOIN director USING(director_id) WHERE director_id=' .
+            $_GET['director_id'] .
+            ' GROUP BY director_id';
+          $title = '🎞 Sales of Films 🎞';
+          $head =
+            "<tr class='head_tr'><th>Average<br>*Total*</th><th>Max<br>*Total*</th><th>Average<br>*Seoul*</th><th>Max<br>*Seoul*</th</tr>";
+        } else {
+          $sql =
+            'SELECT movie_name as c1, released_date as c2 FROM movie JOIN director USING(director_id) WHERE director_id=' .
+            $_GET['director_id'] .
+            ' ORDER BY released_date DESC';
+          $title = '🎞 List of Films 🎞';
+          $head = "<tr class='head_tr'><th>Title</th><th>Released Date</th></tr>";
         }
-        ?>
+        $res = mysqli_query($mysqli, $sql);
+        $director_list = mysqli_fetch_array($res, MYSQLI_ASSOC);
+        if ($res) {
+          printf("<h2 class='toCenter'>%s</h2>", $title);
+          printf("<table class=\"toCenter\">%s", $head);
+          $i = 0;
+          do {
+            $c1 = $director_list['c1'];
+            $c2 = $director_list['c2'];
+            printf(
+              "<tr class=\"normal_tr\"><td style=\"width:400px\"><B> %s </B></td><td style=\"width:400px\"> %s </td>",
+              $c1,
+              $c2
+            );
+            if ((int) $menu > 1) {
+              $c3 = $director_list['c3'];
+              $c4 = $director_list['c4'];
+              printf(
+                "<td style=\"width:400px\"><B> %s </B></td><td style=\"width:400px\"> %s </td>",
+                $c3,
+                $c4
+              );
+            }
+          } while ($director_list = mysqli_fetch_array($res, MYSQLI_ASSOC));
+
+          printf('</table>');
+        } else {
+          printf("Could not get the ranking of directors: %s\n", mysqli_error($mysqli));
+        }
+        mysqli_close($mysqli);
+      }
+      ?>
     </div>
 </section>
 <div id="downdeco">
